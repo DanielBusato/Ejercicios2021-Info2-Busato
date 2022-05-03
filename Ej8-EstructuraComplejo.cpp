408998050eecc8bb1c2b7be0f04fabf3e0bf97fb
DanielBusato <101680120+DanielBusato@users.noreply.github.com>
2022-05-03 20:55:17 -0300

    Update Ej8-EstructuraComplejo.cpp

--- Ej8-EstructuraComplejo.cpp
+++ Ej8-EstructuraComplejo.cpp
@@ -13,14 +13,14 @@ int main(int argc, char *argv[]) {
 	
 	px=&x;
 	
-	cx[18].real=9.9;
-	cx[18].imaginario=8.8;
+	cx[17].real=9.9;
+	cx[17].imaginario=8.8;
 	
 	printf("El miembro real de la variable x es: %.2f\n",x.real);
 	printf("El miembro imaginario de la variable x es: %.2f\n",x.imaginario);
 	
-	printf("El elemento n18 del arreglo cx, miembro real  es: %.2f\n",cx[18].real);
-	printf("El elemento n18 del arreglo cx, miembro imaginario es: %.2f\n",cx[18].imaginario);
+	printf("El elemento n18 del arreglo cx, miembro real  es: %.2f\n",cx[17].real);
+	printf("El elemento n18 del arreglo cx, miembro imaginario es: %.2f\n",cx[17].imaginario);
 	
 	
 	
