6490c2f5eb562f9b19a4cbc5205f954c14c5114d
DanielBusato <101680120+DanielBusato@users.noreply.github.com>
2022-05-03 20:30:33 -0300

    Add files via upload

---
+++ Ej8-EstructuraComplejo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+struct complejo{
+	float real;
+	float imaginario;
+};
+
+int main(int argc, char *argv[]) {
+	struct complejo x, *px, cx[100];
+	
+	x.real=1.3;
+	x.imaginario=-2.2;
+	
+	px=&x;
+	
+	cx[18].real=9.9;
+	cx[18].imaginario=8.8;
+	
+	printf("El miembro real de la variable x es: %.2f\n",x.real);
+	printf("El miembro imaginario de la variable x es: %.2f\n",x.imaginario);
+	
+	printf("El elemento n18 del arreglo cx, miembro real  es: %.2f\n",cx[18].real);
+	printf("El elemento n18 del arreglo cx, miembro imaginario es: %.2f\n",cx[18].imaginario);
+	
+	
+	
+	
+
+	
+	
+	system("pause");
+	return 0;
+}
+
