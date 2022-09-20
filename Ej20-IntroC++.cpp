76a91904df0d31adea26becd34f6b1b3945c0a0e
DanielBusato <101680120+DanielBusato@users.noreply.github.com>
2022-09-20 19:12:36 -0300

    Add files via upload

---
+++ Ej20-IntroC++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	char nombre[10], domicilio[20], ciudad[20], estado[20];
+	int cp;
+
+	std::cout<<"ingrese nombre\n";
+	std::cin>>nombre;
+	std::cout<<"ingrese domicilio\n";
+	std::cin>>domicilio;
+	std::cout<<"ingrese ciudad\n";
+	std::cin>>ciudad;
+	std::cout<<"ingrese estado civil\n";
+	std::cin>>estado;
+	std::cout<<"ingrese codigo postal\n";
+	std::cin>>cp;
+	
+	std::cout<<nombre<<"\n";
+	std::cout<<domicilio<<"\n";
+	std::cout<<ciudad<<" "<<estado<<" "<<cp<<std::endl;
+
+	system("pause");
+	return 0;
+}
+
---
+++ Ej21-PasoReferencia.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int tripleLlamadaPorValor(int a){
+	a=a*3;
+	std::cout<<"Valor triplicado por valor: "<<a<<std::endl;
+	return a;
+}
+void triplePorReferencia(int &a){
+	a=a*3;
+	std::cout<<"Valor triplicado por referencia: "<<a<<std::endl;
+}
+
+int main(int argc, char *argv[]) {
+	int num=2;
+	
+	int nuevovalor=tripleLlamadaPorValor(num);
+	triplePorReferencia(num);
+	
+	system("pause");
+	return 0;
+}
+
